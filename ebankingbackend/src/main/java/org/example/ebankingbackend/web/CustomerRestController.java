6f7cfa843da8cbf3870ae41a411f591c9def2e60
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-05-02 19:59:28 +0000

    make restfule controller for Customer

---
+++ ebankingbackend/src/main/java/org/example/ebankingbackend/web/CustomerRestController.java
@@ -0,0 +1,44 @@
+package org.example.ebankingbackend.web;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.ebankingbackend.dtos.CustomerDTO;
+import org.example.ebankingbackend.entities.Customer;
+import org.example.ebankingbackend.exceptions.CustomerNotFoundException;
+import org.example.ebankingbackend.services.BankAccountService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+public class CustomerRestController {
+    private BankAccountService bankAccountService;
+    @GetMapping("/customers")
+    public List<CustomerDTO> getCustomers(){
+        log.info("Getting all customers");
+        return bankAccountService.listCustomers();
+    }
+    @GetMapping("/customers/{id}")
+    public CustomerDTO getCustomer(@PathVariable(name = "id") Long id)throws CustomerNotFoundException {
+        log.info("Getting customer with id {}", id);
+        return bankAccountService.getCustomer(id);
+    }
+    @PostMapping("/customers")
+    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO){
+        log.info("Saving customer");
+        return bankAccountService.saveCustomer(customerDTO);
+    }
+    @PutMapping("/customers/{customerId}")
+    public CustomerDTO updateCustomer(@PathVariable(name = "customerId") Long customerId,@RequestBody CustomerDTO customerDTO){
+        log.info("Updating customer with id {}", customerId);
+        customerDTO.setId(customerId);
+        return bankAccountService.saveCustomer(customerDTO);
+    }
+    @DeleteMapping("/customers/{customerId}")
+    public void deleteCustomer(@PathVariable(name = "customerId") Long customerId) throws CustomerNotFoundException {
+        log.info("Deleting customer with id {}", customerId);
+        bankAccountService.deleteCustomer(customerId);
+    }
+}
