194056536f817374f32cb409500c2e0ad9787cb7
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-05-02 20:00:01 +0000

    Add BankService

---
+++ ebankingbackend/src/main/java/org/example/ebankingbackend/services/BankService.java
@@ -0,0 +1,9 @@
+package org.example.ebankingbackend.services;
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class BankService {
+}
