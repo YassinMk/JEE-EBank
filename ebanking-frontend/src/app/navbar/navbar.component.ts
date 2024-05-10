ffd351a8d5a581b3c5ff8acdbbd655fec4ebac19
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-05-10 23:24:52 +0000

    feat: Update admin customer links in navbar component

--- ebanking-frontend/src/app/navbar/navbar.component.ts
+++ ebanking-frontend/src/app/navbar/navbar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  constructor(public authService: AuthService, private router: Router) {
+  }
+  ngOnInit(): void {
 
+  }
+
+  handleLogout() {
+    this.authService.logout();
+
+  }
 }
