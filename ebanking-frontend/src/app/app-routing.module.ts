1722a629133efbbabf30d6c25cdeb020390a65f6
YassinMk <122708120+YassinMk@users.noreply.github.com>
2024-05-10 23:30:21 +0000

    feat: Add login route and components for user authentication

--- ebanking-frontend/src/app/app-routing.module.ts
+++ ebanking-frontend/src/app/app-routing.module.ts
@@ -4,12 +4,22 @@ import {CustomersComponent} from "./customers/customers.component";
 import {AccountsComponent} from "./accounts/accounts.component";
 import {NewCustomersComponent} from "./new-customers/new-customers.component";
 import {CustomerAccountsComponent} from "./customer-accounts/customer-accounts.component";
+import {LoginComponent} from "./login/login.component";
+import {AdminTemplateComponent} from "./admin-template/admin-template.component";
+import {AuthentificationGuard} from "./guards/authentification.guard";
+import {AuthorizationGuard} from "./guards/authorization.guard";
+import {NotAuthorizedComponent} from "./not-authorized/not-authorized.component";
 
 const routes: Routes = [
-  {path:"customers",component:CustomersComponent},
-  {path:"accounts",component:AccountsComponent},
-  {path:"new-customers",component:NewCustomersComponent},
-  {path:"customer-accounts/:id",component:CustomerAccountsComponent}
+  {path:"login", component: LoginComponent},
+  {path:"",redirectTo:"login",pathMatch:"full"},
+  {path:"admin",component:AdminTemplateComponent,canActivate:[AuthentificationGuard],children:[
+      {path:"customers",component:CustomersComponent},
+      {path:"accounts",component:AccountsComponent},
+      {path:"new-customers",component:NewCustomersComponent,canActivate:[AuthorizationGuard],data:{role:"ADMIN"}},
+      {path:"customer-accounts/:id",component:CustomerAccountsComponent},
+      {path:"notAuthorized",component:NotAuthorizedComponent}
+  ]}
 ];
 
 @NgModule({
